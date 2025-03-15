da8854763ab74b05043a75274baea1afcd490e6f
Mellon Obada <mellobadah123@gmail.com>
2025-03-15 11:09:53 +0300

    array-find

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+  ]
+  function superbowlWin(record) {  
+    function checkWin(game) {  
+        return game.result === "W";
+    }
+
+    const bowlWin = record.find(checkWin);  
+
+    if (bowlWin) {
+        return bowlWin.year;  
+    } else {
+        return undefined;
+    }
+}
